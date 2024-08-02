ab634486f45ca72fdaf72902b9e69cf263736ccd
Saaj-0809 <saazmulik2005@gmail.com>
2024-08-02 09:06:56 +0530

    for loop.cpp

--- for loop.cpp
+++ for loop.cpp
@@ -14,3 +14,22 @@ int main()
     }
     return 0;
 }
+
+/* OUTPUT 
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+
+--------------------------------
+Process exited after 0.08315 seconds with return value 0
+Press any key to continue . . . */
